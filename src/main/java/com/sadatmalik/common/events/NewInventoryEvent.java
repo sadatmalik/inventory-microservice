5fe3614e05c81c418c7a1110c6168d609096a183
sadat.malik <sadat.malik@me.com>
2022-02-10 15:01:48 +0000

    Adding NewInventoryListener and common events package

---
+++ src/main/java/com/sadatmalik/common/events/NewInventoryEvent.java
@@ -0,0 +1,10 @@
+package com.sadatmalik.common.events;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class NewInventoryEvent extends BeerEvent {
+    public NewInventoryEvent(BeerDto beerDto) {
+        super(beerDto);
+    }
+}
---
+++ src/main/java/com/sadatmalik/inventoryservice/services/NewInventoryListener.java
@@ -0,0 +1,31 @@
+package com.sadatmalik.inventoryservice.services;
+
+import com.sadatmalik.common.events.NewInventoryEvent;
+import com.sadatmalik.inventoryservice.config.JmsConfig;
+import com.sadatmalik.inventoryservice.domain.BeerInventory;
+import com.sadatmalik.inventoryservice.repositories.BeerInventoryRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class NewInventoryListener {
+
+    private final BeerInventoryRepository beerInventoryRepository;
+
+    @JmsListener(destination = JmsConfig.NEW_INVENTORY_QUEUE)
+    public void listen(NewInventoryEvent event){
+
+        log.debug("Got Inventory: " + event.toString());
+
+        beerInventoryRepository.save(BeerInventory.builder()
+                .beerId(event.getBeerDto().getId())
+                .upc(event.getBeerDto().getUpc())
+                .quantityOnHand(event.getBeerDto().getQuantityOnHand())
+                .build());
+    }
+
+}
