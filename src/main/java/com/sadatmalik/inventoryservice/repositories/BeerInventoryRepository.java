f1a2efb86d64208bd9d87a3e0b6753281a49f16a
sadat.malik <sadat.malik@me.com>
2022-02-11 11:33:55 +0000

    Adding allocation service

--- src/main/java/com/sadatmalik/inventoryservice/repositories/BeerInventoryRepository.java
+++ src/main/java/com/sadatmalik/inventoryservice/repositories/BeerInventoryRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.UUID;
 
 public interface BeerInventoryRepository extends JpaRepository<BeerInventory, UUID> {
-
     List<BeerInventory> findAllByBeerId(UUID beerId);
+
+    List<BeerInventory> findAllByUpc(String upc);
 }
---
+++ src/main/java/com/sadatmalik/inventoryservice/services/AllocationService.java
@@ -0,0 +1,8 @@
+package com.sadatmalik.inventoryservice.services;
+
+import com.sadatmalik.brewery.model.BeerOrderDto;
+
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+}
